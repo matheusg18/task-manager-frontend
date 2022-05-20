d2753e1502053e7c94283b964470651131d090a7
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-19 21:20:27 -0300

    feat: add task list as state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
 import { Heading, VStack } from '@chakra-ui/react';
 import AddTask from '../components/AddTask';
+import { ITask } from '../interfaces/api/task';
 
 const Home: NextPage = () => {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
+  const addTask = (newTask: ITask) => {
+    setTaskList((prev) => [...prev, newTask]);
+  };
+
   return (
     <main>
       <VStack>
         <Heading as="h1" size="2xl">
           Ebtyr Task Manager
         </Heading>
-        <AddTask />
+        <AddTask addTask={addTask} />
       </VStack>
     </main>
   );
